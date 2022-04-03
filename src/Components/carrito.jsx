f454a4021e4bc952fb53be4de72da424c9a943aa
aguscara2001 <aguscvara2001@gmail.com>
2022-04-03 01:06:34 -0300

    commit-carrito

---
+++ src/Components/carrito.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default function Carrito(){
+    return(
+       <button><img src="https://cdn-icons.flaticon.com/png/512/4175/premium/4175270.png?token=exp=1648959446~hmac=59107a97f2a4ca7f35290119a2c09424" alt="carrito-compras" height={30}/></button>
+    )
+
+}
